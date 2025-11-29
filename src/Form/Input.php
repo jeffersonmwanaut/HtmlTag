0bddcdd1ad4da97414bb9bcecec2d9c93c30d4ee
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2025-11-29 15:39:04 +0200

    Update Input.php

--- src/Form/Input.php
+++ src/Form/Input.php
@@ -3,11 +3,11 @@ namespace HtmlTag\Form;
 
 class Input extends FormControl
 {
-    public function __construct(string $type = 'text', string $name = '', string $id = '', string $value = '') {
+    public function __construct(string $type = 'text', string $name = null, string $id = null, string $value = null) {
         parent::__construct(tagName: 'input', name: $name, id: $id);
         $this->attr('type', strtolower($type));
         $this->attr('placeholder', "Enter {$this->humanize($name)}");
-        if ($value !== '') {
+        if ($value !== null) {
             $this->attr('value', $value);
         }
     }
