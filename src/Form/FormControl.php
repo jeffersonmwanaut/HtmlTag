01ea49e7663ef155b9e0797484388e7cbef4ccdb
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2025-11-29 15:39:01 +0200

    Update FormControl.php

--- src/Form/FormControl.php
+++ src/Form/FormControl.php
@@ -5,14 +5,20 @@ use HtmlTag\HtmlTag;
 
 class FormControl extends HtmlTag
 {
-    public function __construct(string $tagName, string $name = '', string $id = '') 
+    public function __construct(string $tagName, string $name = null, string $id = null) 
     {
         parent::__construct($tagName);
-        if ($name !== '') {
+        if ($name !== null) {
             $this->attr('name', $name);
+        } elseif ($id !== null) {
+            $this->attr('name', $id);
         }
-        if ($id !== '') {
+
+        if ($id !== null) {
             $this->attr('id', $id);
+            $this->attr('autocomplete', 'on');
+        } elseif ($name !== null) {
+            $this->attr('id', $name);
             $this->attr('autocomplete', 'on');
         }
     }
