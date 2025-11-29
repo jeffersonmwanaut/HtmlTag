99715710b85818846dbf9e813efacf5fc955fc31
Jefferson Mwanaut <83014667+jeffersonmwanaut@users.noreply.github.com>
2025-11-29 15:39:08 +0200

    Update Label.php

--- src/Form/Label.php
+++ src/Form/Label.php
@@ -5,10 +5,11 @@ use HtmlTag\HtmlTag;
 
 class Label extends HtmlTag
 {
-    public function __construct(string $for, string $text = '') 
+    public function __construct(string $for, string $text = null) 
     {
         parent::__construct('label');
         $this->attr('for', $for);
+        $text = $text ?? $for;
         $this->appendText($this->humanize($text));
     }
 }
